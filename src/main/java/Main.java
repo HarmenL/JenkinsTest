679043e259d53cb1c8ccbfadab8ebb8159846bac
HarmenL <mail@harmenleusink.nl>
2018-12-11 00:46:09 +0200

    Started project

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Name myName = new Name("Harmen");
+        System.out.println(myName.WhatIsMyName());
+    }
+}
---
+++ src/main/java/Name.java
@@ -0,0 +1,14 @@
+public class Name {
+
+    private String savedName;
+
+    public Name(String newName)
+    {
+        savedName = newName;
+    }
+
+    public String WhatIsMyName()
+    {
+        return savedName;
+    }
+}
